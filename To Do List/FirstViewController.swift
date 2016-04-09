9772a0cc09c0acc28e84293745f4eeb35e668108
Lawrence Chen <lawrencechen@Lawrences-Mac.local>
2016-04-08 20:57:28 -0500

    Initial commit

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,17 +8,94 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+// Initializing variable outside of class so it can be
+// used by the SecondViewController 
 
+// Define our array for us as soon as the app is loaded
+var toDoList = [String]()
+
+var contactInfo = [String]()
+
+
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet var toDoListTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if(NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil){
+        
+        toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+            
+        }
+        
+        if(NSUserDefaults.standardUserDefaults().objectForKey("contactInfo") != nil){
+            
+            contactInfo = NSUserDefaults.standardUserDefaults().objectForKey("contactInfo") as! [String]
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return toDoList.count
+        
+    }
+    
+    
+    // Cells prototype gets identified by its identifier "cell" and then the cell's text gets set
+    // by using the toDoList array and getting the indexPath.row
+    // finally ---- return cell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell = 	UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    // Need to update value in table every time the above view appears
+    // so this can be down with 
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        // Updating the table with reloadData()
+        toDoListTable.reloadData()
+        
+    }
+    
+    
+    // SWIPE LEFT TO DELETE METHOD! -----------------------------------
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        
+        // To check for a swipe to the left
+        if (editingStyle == UITableViewCellEditingStyle.Delete){
+        
+            // Updates array version
+            toDoList.removeAtIndex(indexPath.row)
+            
+            // Updates the perm storage version
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            
+            toDoListTable.reloadData()
+            
+        }
+        
+    }
+    
+    
 
 
 }
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,16 +8,89 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var itemField: UITextField!
+    @IBOutlet var addressField: UITextField!
+    @IBOutlet var cityField: UITextField!
+    @IBOutlet var stateField: UITextField!
+    @IBOutlet var zipField: UITextField!
+    @IBOutlet var cellField: UITextField!
+    @IBOutlet var homeField: UITextField!
+    
+    @IBAction func captureImage() {
+        
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        
+        presentViewController(picker, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func takePicture() {
+        
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .Camera
+        
+        presentViewController(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        imageView.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        // Adding the item put into the text field to the array
+        toDoList.append(itemField.text!)
+        
+        
+        // Clearing item.text so that the textfield will be empty and they can write another to do list item
+        itemField.text = ""
+        addressField.text = ""
+        cityField.text = ""
+        stateField.text = ""
+        zipField.text = ""
+        cellField.text = ""
+        homeField.text = ""
+        
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        itemField.resignFirstResponder()
+        
+        return true
+        
     }
 
 
